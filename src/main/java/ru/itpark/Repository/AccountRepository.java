ff65f61dda9bbf2f6e6bc8d7ba5e3b302b72f6df
Student01 <student@localhost>
2018-11-11 15:41:47 +0300

    Initial commit

---
+++ src/main/java/ru/itpark/Repository/AccountRepository.java
@@ -0,0 +1,15 @@
+package ru.itpark.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountRepository {
+    private List <Account> accounts = new ArrayList<Account>();
+    private int nextld = 1;
+
+
+
+    public  Account add(Account account){
+        account
+    }
+}
